0126c74786d7c54bf750cca5835574b0409171ca
Cryptopod01 <azuredrake87@gmail.com>
2024-06-13 20:03:05 -0700

    Just a Template

---
+++ src/main/java/net/zuredragon/templatemod/TemplateModClient.java
@@ -0,0 +1,10 @@
+package net.zuredragon.templatemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TemplateModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
