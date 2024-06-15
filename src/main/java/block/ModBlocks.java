36dd9310da29263956c6e8262cbbe6be39ee3d77
Cryptopod01 <azuredrake87@gmail.com>
2024-06-14 20:53:15 -0700

    Missing texture of death

--- src/main/java/block/ModBlocks.java
+++ src/main/java/block/ModBlocks.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.MapColor;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
@@ -13,11 +14,11 @@ import net.zuredragon.templatemod.TemplateMod;
 
 public class ModBlocks {
     public static final Block SILVERORE = registerBlock("silverore",
-            new Block(FabricBlockSettings.copyOf(Blocks.IRON_ORE)));
+            new Block(FabricBlockSettings.copyOf(Blocks.IRON_ORE).mapColor(MapColor.STONE_GRAY)));
     public static final Block RAWSILVERBLOCK = registerBlock("rawsilverblock",
-            new Block(FabricBlockSettings.copyOf(Blocks.RAW_IRON_BLOCK)));
+            new Block(FabricBlockSettings.copyOf(Blocks.RAW_IRON_BLOCK).mapColor(MapColor.TERRACOTTA_CYAN)));
     public static final Block SILVERBLOCK = registerBlock("silverblock",
-            new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK)));
+            new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).mapColor(MapColor.TERRACOTTA_CYAN)));
 
     private static Block registerBlock(String name, Block block){
         registerBlockItem(name, block);
